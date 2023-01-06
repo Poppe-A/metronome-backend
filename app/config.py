525f363b45130e7b03f7f98e0896697fc2728caa
Antoine Poppe <antoinepoppe@hotmail.fr>
2023-01-06 10:53:12 +0100

    Test port

--- app/config.py
+++ app/config.py
@@ -6,5 +6,5 @@ class Config(object) :
     FLASK_DEBUG = os.getenv('DEBUG', True)
     DB_USER = os.getenv('DB_USER', 'postgres')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
-    DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+    DB_HOST = os.getenv('DB_HOST', 'localhost:2345')
     DB_NAME = os.getenv('DB_NAME', 'logme')
